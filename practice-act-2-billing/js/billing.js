fa3f2a3bab1db3124fce099f343cbc8a0b7310b5
Fonsoooo <alfonsogabriel.millares.iics@ust.edu.ph>
2020-02-15 23:51:33 +0800

    git commit .

---
+++ practice-act-2-billing/js/billing.js
@@ -0,0 +1,10 @@
+/*Add the JavaScript here for the function billingFunction().  It is responsible for setting and clearing the fields in Billing Information */
+
+function billingFunction()
+{
+    var name =  document.getElementById("shippingName").value;    
+    var zip =   document.getElementById("shippingZip").value;
+    
+    document.getElementById("billingName").value = name;
+    document.getElementById("billingZip").value = zip;
+}
