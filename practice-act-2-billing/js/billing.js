844797dae809104aa5ea99aba063df35f915caf3
Fonsoooo <alfonsogabriel.millares.iics@ust.edu.ph>
2020-02-15 23:52:47 +0800

    test update

--- practice-act-2-billing/js/billing.js
+++ practice-act-2-billing/js/billing.js
@@ -1,5 +1,6 @@
 /*Add the JavaScript here for the function billingFunction().  It is responsible for setting and clearing the fields in Billing Information */
 
+
 function billingFunction()
 {
     var name =  document.getElementById("shippingName").value;    
