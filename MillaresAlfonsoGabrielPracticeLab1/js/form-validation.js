f9b203d69c9ead26defacce4695f30a2f6e7cfa6
Fonsoooo <alfonsogabriel.millares.iics@ust.edu.ph>
2020-02-24 07:31:52 +0800

    [edited form-vali1 js. still not complete]

--- MillaresAlfonsoGabrielPracticeLab1/js/form-validation.js
+++ MillaresAlfonsoGabrielPracticeLab1/js/form-validation.js
@@ -1,6 +1,7 @@
 function validateForm() {
     fullDate1 = document.getElementById("eDate1ID").value;
     fullDate2 = document.getElementById("eDate2ID").value;
+    fullDate3 = document.getElementById("pDateID").value;
 
     year1 =  fullDate1.substring(0, 4);
     month1 = fullDate1.substring(5, 7);
@@ -10,20 +11,63 @@ function validateForm() {
     month2 = fullDate2.substring(5, 7);
     day2 =   fullDate2.substring(8, 10);
 
+    year3 =  fullDate3.substring(0, 4);
+    month3 = fullDate3.substring(5, 7);
+    day3 =   fullDate3.substring(8, 10);
+
     fullDate1F = {year1, month1, day1};
     fullDate2F = {year2, month2, day2};
+    fullDate3F = {year3, month3, day3};
 
+    //check event dates
 
     if(fullDate1F.year1 <= fullDate2F.year2){
-        if(fullDate1F.month1 <= fullDate2F.month2){
-            if(fullDate1F.day1 <= fullDate2F.day2){
-                alert("Submission Received!");
-            }
-            else alert("Date not Valid! Starting Date must be before or on the same date as End Date. [Day Invalid]");
+        if(fullDate1F.year1 < fullDate2F.year2){ 
+            checkPost();
         }
-        else alert("Date not Valid! Starting Date must be before or on the same date as End Date. [Month Invalid]");
+        else if(fullDate1F.month1 <= fullDate2F.month2){
+            if(fullDate1F.month1 < fullDate2F.month2){
+                checkPost();
+            }
+            else if(fullDate1F.day1 <= fullDate2F.day2){
+                checkPost();
+            }
+            else alert("Event Dates not Valid! Starting Date must be before or on the same date as End Date. [Day Invalid]");
+        }
+        else alert("Event Dates not Valid! Starting Date must be before or on the same date as End Date. [Month Invalid]");
     }
-    else alert("Date not Valid! Starting Date must be before or on the same date as End Date. [Year Invalid]");
+    else alert("Event Dates not Valid! Starting Date must be before or on the same date as End Date. [Year Invalid]");
+
+    function checkPost(){
+        // check posting date
+
+        if(fullDate3F.year3 >= fullDate1F.year1 && fullDate3F.year3 <= fullDate2F.year2){
+            if(fullDate3F.year3 == fullDate2F.year2){
+                if(fullDate3F.month3 <= fullDate2F.month2){
+                    if(fullDate3F.day3 <= fullDate2F.day2){
+                        alert("Submission Received!");
+                    } 
+                }
+                
+                
+            }
+    
+            else if(fullDate3F.month3 >= fullDate1F.month1 && fullDate3F.month3 <= fullDate2F.month2){
+                if(fullDate3F.month3 > fullDate1F.month11 && fullDate3F.month3 < fullDate2F.month2){ 
+                    alert("Submission Received!");
+                }
+                
+                else if(fullDate3F.day3 >= fullDate1F.day1 && fullDate3F.day3 <= fullDate2F.day2){
+                    alert("Submission Received!");
+                }
+                else alert("Posting Date not Valid! Posting Date must be between Starting and End Dates. [Day Invalid]");
+            }
+            else alert("Posting Date not Valid! Posting Date must be between Starting and End Dates. [Month Invalid]");
+        }
+        else alert("Posting Date not Valid! Posting Date must be between Starting and End Dates. [Year Invalid]");
+    }
+
+    
     
     date1 = document.getElementById("eDate1ID").value;
     alert(year + " " + month + " " + day);
